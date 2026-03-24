fe357b7de3b32ea1490df10794ec4117a70a63b0
Mihatel <TelyatnikovMA@yandex.ru>
2026-03-24 18:04:51 +0500

    8 task

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,17 +1,31 @@
 const express = require('express');
 const router = express.Router();
 
+const sqlite3 = require('sqlite3').verbose()
+const db = new sqlite3.Database('mydb.db');
+db.run(`CREATE TABLE IF NOT EXISTS users (
+   id INTEGER PRIMARY KEY AUTOINCREMENT,
+   name text)`);
+
 const users = [];
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.json({ items: users });
+  db.all("SELECT id, name FROM users", [], (err, rows) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(rows);
+    }
+  });
 });
 
 router.post('/', function(req, res, next) {
-  const newUser = req.body;
-  users.push(newUser);
-  res.status(201).json(newUser);
+
+  const name = req.body["name"];
+  const insert = "INSERT INTO users (name) VALUES (?)";
+  db.run(insert, [name]);
+  res.status(201).json(name);
 })
 
 module.exports = router;
